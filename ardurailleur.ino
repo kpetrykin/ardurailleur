8707fcbc074b44e67a9ec2fcdd7852d79b6055f4
izyum <kpetrykin@gmail.com>
2020-01-04 00:12:32 +0300

    Enables internal voltmeter and adds uV string to display

--- ardurailleur.ino
+++ ardurailleur.ino
@@ -75,6 +75,31 @@ void save_to_EEPROM()
   }
 }
 
+// Internal voltmeter reading
+long readVcc() {
+  // Read 1.1V reference against AVcc
+  // set the reference to Vcc and the measurement to the internal 1.1V reference
+  #if defined(__AVR_ATmega32U4__) || defined(__AVR_ATmega1280__) || defined(__AVR_ATmega2560__)
+    ADMUX = _BV(REFS0) | _BV(MUX4) | _BV(MUX3) | _BV(MUX2) | _BV(MUX1);
+  #elif defined (__AVR_ATtiny24__) || defined(__AVR_ATtiny44__) || defined(__AVR_ATtiny84__)
+     ADMUX = _BV(MUX5) | _BV(MUX0) ;
+  #else
+    ADMUX = _BV(REFS0) | _BV(MUX3) | _BV(MUX2) | _BV(MUX1);
+  #endif  
+ 
+  delay(2); // Wait for Vref to settle
+  ADCSRA |= _BV(ADSC); // Start conversion
+  while (bit_is_set(ADCSRA,ADSC)); // measuring
+ 
+  uint8_t low  = ADCL; // must read ADCL first - it then locks ADCH  
+  uint8_t high = ADCH; // unlocks both
+ 
+  long result = (high<<8) | low;
+ 
+  result = 1125300L / result; // Calculate Vcc (in mV); 1125300 = 1.1*1023*1000
+  return result; // Vcc in millivolts
+}
+
 void loop()
 {
   uView.clear(PAGE);
@@ -148,5 +173,7 @@ void loop()
   uView.setCursor(0, 10);
   uView.print("angle: ");
   uView.print(gears[current_gear]);
+  uView.setCursor(0, 30);
+  uView.print("uV: "); uView.print(readVcc());
   uView.display();
 }
